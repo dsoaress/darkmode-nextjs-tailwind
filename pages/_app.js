08d5ca860a5c3b9956e99ed5d4c857fc5171eda4
Daniel Soares <daniel.soares@me.com>
2021-05-18 23:57:05 -0300

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "next-themes";
+
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
